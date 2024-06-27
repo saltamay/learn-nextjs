2c391c3a38431746c32ab5e3bf19fcd201cf9725
Sal Tamay <sal.tamay@outlook.com>
2024-06-27 19:26:42 -0400

    Add navigation and setup page structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,14 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "./ui/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "React and Next.js Playground",
-  description: "Learn React and Next.js with an interactive browser-based tutorial",
+  description:
+    "Learn React and Next.js with an interactive browser-based tutorial",
 };
 
 export default function RootLayout({
@@ -16,7 +18,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-gray-900 text-gray-100`}>{children}</body>
+      <body className={`${inter.className}  bg-gray-900 text-gray-100`}>
+        <Navbar />
+        <div className="flex h-[calc(100vh-64px)] flex-col md:flex-row md:overflow-hidden">
+          <div className="w-full flex-none md:w-1/3 border md:p-12 border-gray-100">
+            Content
+          </div>
+          <div className="flex-grow p-6 md:overflow-y-auto md:p-12 border border-gray-100">
+            {children}
+          </div>
+        </div>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <main >
-      Home Page
-    </main>
-  );
+  return <main>Interactive Playground</main>;
 }
