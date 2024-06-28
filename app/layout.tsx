7a2cc9967e4f9b4a5e043eaa4f246b9b99042bd8
Sal Tamay <sal.tamay@outlook.com>
2024-06-28 12:19:52 -0400

    Add resizable panel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,12 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "./ui/navbar";
+import Navbar from "../components/navbar";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,16 +23,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className}  bg-gray-900 text-gray-100`}>
+      <body className={`${inter.className}  bg-[#23272f] text-gray-100`}>
         <Navbar />
-        <div className="flex h-[calc(100vh-64px)] flex-col md:flex-row md:overflow-hidden">
-          <div className="w-full flex-none md:w-1/3 border md:p-12 border-gray-100">
-            Content
-          </div>
-          <div className="flex-grow p-6 md:overflow-y-auto md:p-12 border border-gray-100">
-            {children}
-          </div>
-        </div>
+        <ResizablePanelGroup
+          direction="horizontal"
+          className="min-h-[200px] border"
+        >
+          <ResizablePanel defaultSize={35} className="h-[calc(100vh-64px)]">
+            <div className="flex h-full items-center justify-center font-semibold">
+              Content
+            </div>
+          </ResizablePanel>
+          <ResizableHandle withHandle />
+          <ResizablePanel defaultSize={65}>
+            <div className="flex h-full items-center justify-center">
+              {children}
+            </div>
+          </ResizablePanel>
+        </ResizablePanelGroup>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
-  return <main>Interactive Playground</main>;
+  return <main className="font-semibold">Playground</main>;
 }
